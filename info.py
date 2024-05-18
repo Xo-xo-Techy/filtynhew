09aa21d240d026428994020ef8c1f85885f6ef3d
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-18 20:00:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-100153936681'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001539366814'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '945284066').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001747774577').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
