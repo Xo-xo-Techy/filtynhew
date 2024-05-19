d146041f2158069098a4fe072006f818d069ff19
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-19 09:28:00 +0530

    Update stream.py

--- plugins/Extra/stream/stream.py
+++ plugins/Extra/stream/stream.py
@@ -7,6 +7,10 @@ from TechVJ.util.human_readable import humanbytes
 import humanize
 import random
 
+#fligher
+from Tech_VJ.vars import Var
+from pyshorteners import Shortener
+
 @Client.on_message(filters.private & filters.command("stream"))
 async def stream_start(client, message):
     if STREAM_MODE == False:
@@ -29,22 +33,25 @@ async def stream_start(client, message):
         fileName = {quote_plus(get_name(log_msg))}
         stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+
+        short_link= await get_shortlink(stream)
+        shorto_link= await get_shortlink(download)
  
         await log_msg.reply_text(
             text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
-                                                InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=shorto_link),  # we download Link
+                                                InlineKeyboardButton('🖥️ Watch online 🖥️', url=short_link)]])  # web stream Link
         )
         rm=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
-                    InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 📥', url=download)
+                    InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=short_link),
+                    InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 📥', url=shorto_link)
                 ]
             ] 
         )
         msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
 
-        await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)
+        await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), shorto_link, short_link), quote=True, disable_web_page_preview=True, reply_markup=rm)
