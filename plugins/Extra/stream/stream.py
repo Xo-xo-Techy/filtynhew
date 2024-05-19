eb71ab4d9a3c6f11e391b9d07a735f10a3ce56f1
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-19 17:07:24 +0530

    Update stream.py

--- plugins/Extra/stream/stream.py
+++ plugins/Extra/stream/stream.py
@@ -1,17 +1,19 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-from info import STREAM_MODE, URL, LOG_CHANNEL
+from info import STREAM_MODE, URL, LOG_CHANNEL,ADMINS
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 from TechVJ.util.human_readable import humanbytes
 import humanize
 import random
 
-#fligher
-# from TechVJ.util.vars import Var
-# from pyshorteners import Shortener
+fligher
+from TechVJ.util.vars import Var
+from pyshorteners import Shortener
 
-@Client.on_message(filters.private & filters.command("stream"))
+
+# for admins only by @fligher
+@Client.on_message(filters.command("stream") & filters.user(ADMINS))
 async def stream_start(client, message):
     if STREAM_MODE == False:
         return 
@@ -55,3 +57,50 @@ async def stream_start(client, message):
         msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
 
         await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)
+
+
+#for Normal Users
+@Client.on_message(filters.private & filters.command("stream"))
+async def streams_start(client, message):
+    if STREAM_MODE == False:
+        return 
+    msg = await client.ask(message.chat.id, "**Now send me your file/video to get stream and download link**")
+    if not msg.media:
+        return await message.reply("**Please send me supported media.**")
+    if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
+        file = getattr(msg, msg.media.value)
+        filename = file.file_name
+        filesize = humanize.naturalsize(file.file_size) 
+        fileid = file.file_id
+        user_id = message.from_user.id
+        username =  message.from_user.mention 
+
+        log_msg = await client.send_cached_media(
+            chat_id=LOG_CHANNEL,
+            file_id=fileid,
+        )
+        fileName = {quote_plus(get_name(log_msg))}
+        stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+        download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+
+        short_link= await get_shortlink(stream)
+        shorto_link= await get_shortlink(download)
+ 
+        await log_msg.reply_text(
+            text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}\n\n You Are a Normal User You Can't Get Direct Link Sorry Contact @wishsearch_bot or click /plan",
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=shorto_link),  # we download Link
+                                                InlineKeyboardButton('🖥️ Watch online 🖥️', url=short_link)]])  # web stream Link
+        )
+        rm=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=short_link),
+                    InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 📥', url=shorto_link)
+                ]
+            ] 
+        )
+        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
+
+        await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), shorto_link, short_link), quote=True, disable_web_page_preview=True, reply_markup=rm)
