723f79de41d620ab015886fed8bcc5d6e2124182
Xo-xo-Techy <beautiful.shapely.photographer@ghostlymail.com>
2024-11-06 16:39:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TRUMBOTCHAT') # Support Chat Link Wi
 
 # True Or False
 PM_SEARCH = bool(environ.get('PM_SEARCH', True)) # In Pm Search Currently Spell Check Doesn't Work.
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
 VERIFY = bool(environ.get('VERIFY', True))
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
